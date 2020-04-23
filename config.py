dc806e82d30b42067a2e42a8bf319a0475686d40
Emmanuel <aadewale123@gmail.com>
2020-04-23 09:58:46 -0400

    danger

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # Bot Configuration
 # Make sure you paste your 'server_id' as an integer and not a string
-bot_token = "TOKEN"
-server_id = ID
+bot_token = "NzAyODc0Mzc5ODI2MjMzNDY0.XqGacQ.YRKNtIx4E9RBVdW00eEv0P3kQaA"
+server_id = 702873874127388772
 
 category_prefix = "SierraOne"
 channel_prefix = "sierra-hotel-"
 
 # Mega Configuration (OPTIONAL)
-# Leave this as it is if you'd rather have the large files to be 
+# Leave this as it is if you'd rather have the large files to be
 # split into 7.5 MB chunks and sent over Discord
 mega_email = ""
 mega_password = ""
